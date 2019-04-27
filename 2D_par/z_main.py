7daa72a516b4a49e27a52648a257e8a800d0f1f9
abbeyjohnson1 <alj314@gmail.com>
2019-04-27 16:58:11 -0400

    work on main mpi file

--- 2D_par/z_main.py
+++ 2D_par/z_main.py
@@ -1,1 +1,47 @@
+# Abbey L. Johnson
 
+# MAIN PROGRAM
+
+# imports
+from mpi4py import MPI
+from z_mainMR import MASTER
+from z_mainWR import WORKER
+
+# >>> startup mpi >>>
+
+# defines the default communicator, which contains all processes
+comm = MPI.COMM_WORLD
+
+# nPROC specified at mpirun/mpiexec
+# nPROC = number of processes = nWRs +1
+nPROC = comm.Get_size()
+
+# returns process ID of current process; number between 0 and nPROC - 1
+# myID = rank of process
+myID = comm.Get_rank()
+
+# master rank
+mster = 0
+
+# number of workers
+nWRs = nPROC - 1
+
+print('>>> main >>> running on {} workers, myID = {}' .format(nWRs, myID))
+
+# start 0, 1, ... , nWRs tasks
+if (myID == mster):
+    # start CPU timer on master
+    tt0 = MPI.Wtime()
+    # call master
+    MASTER()
+    # end timer
+    tt1 = MPI.Wtime(comm)
+    # calculate run time
+    tt = tt1 - tt0
+    print('>>> main >>> master timing = {} seconds on {} workers' .format(tt, nWRs))
+else:
+    # call worker, now MPI is running
+    WORKER(comm)
+    print('bye-bye from worker number {}' .format(myID))
+
+# <<< end mpi <<<
